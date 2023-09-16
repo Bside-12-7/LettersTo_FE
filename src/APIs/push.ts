fc85e903a18c24381ac48d3da1759448ae37a8fe
kkjjss <jinsukk9763@gmail.com>
2023-09-16 18:08:53 +0900

    푸시 등록 기능 추가

---
+++ src/APIs/push.ts
@@ -0,0 +1,13 @@
+import {axiosInstance} from '@utils/http';
+
+interface RegisterPushTokenRequestParams {
+  type: 'APNS' | 'FCM';
+  token: string;
+  deviceId: string;
+}
+
+export const registerPushNotificationToken = async (
+  data: RegisterPushTokenRequestParams,
+) => {
+  return await axiosInstance.post('/push/register', data);
+};
--- src/Screens/Main/MainScreen.tsx
+++ src/Screens/Main/MainScreen.tsx
@@ -5,8 +5,11 @@ import {getUserInfo} from '@apis/member';
 import {BottomTab} from '@components/BottomTab/BottomTab';
 import {LetterBoxList} from '../LetterBox/LetterBoxList';
 import {Home} from './HomeScreen';
+import messaging from '@react-native-firebase/messaging';
+import deviceInfo from 'react-native-device-info';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import type {StackParamsList} from '@type/stackParamList';
+import {registerPushNotificationToken} from '@apis/push';
 
 type Props = NativeStackScreenProps<StackParamsList, 'Main'>;
 
@@ -14,6 +17,31 @@ export const Main = ({navigation}: Props) => {
   const [selectedScreen, setSelectedScreen] = useState<'Home' | 'LetterBox'>(
     'Home',
   );
+
+  async function requestPushNotificationPermission() {
+    const authStatus = await messaging().requestPermission();
+    return (
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL
+    );
+  }
+
+  async function registerPushToken() {
+    const pushNotificationPermissionResult =
+      await requestPushNotificationPermission();
+    if (pushNotificationPermissionResult === true) {
+      const token = await messaging().getAPNSToken();
+      const deviceId = await deviceInfo.getUniqueId();
+      if (token && deviceId) {
+        const result = await registerPushNotificationToken({
+          type: 'APNS',
+          token,
+          deviceId,
+        });
+        console.log(result);
+      }
+    }
+  }
 
   const goToHome = useCallback(() => {
     setSelectedScreen('Home');
@@ -32,6 +60,10 @@ export const Main = ({navigation}: Props) => {
 
     fetchUserInfo();
   }, [queryClient]);
+
+  useEffect(() => {
+    registerPushToken();
+  }, []);
 
   return (
     <View style={{flex: 1}}>
