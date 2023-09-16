98c59f84a9a3b64edeae162372cba97a18e7c6f3
kkjjss <jinsukk9763@gmail.com>
2023-09-16 19:52:17 +0900

    공유 이미지 주소 변경

--- src/Constants/share.ts
+++ src/Constants/share.ts
@@ -2,8 +2,7 @@ export const KAKAO_LINK_INVITATION_DATA = {
   content: {
     title: '[Letters to] 진짜 나와 너로 만나는 익명 사서함',
     description: '진짜 하고싶은 말을 편지에 담아 보내세요.',
-    imageUrl:
-      'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+    imageUrl: 'https://lettersto.s3.ap-northeast-2.amazonaws.com/main.png',
     link: {
       mobileWebUrl: 'https://lettersto.onelink.me/DSlL/axeijmx1',
     },
